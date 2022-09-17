a99709e952ec9844bd6b3d7f330d0058f96e6ca6
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-09-17 15:13:55 +0300

    feat: Login Added

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {useDispatch} from "react-redux"
+import { login } from "../stores/auth"
+import {useNavigate} from "react-router-dom"
 
 function Login() {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  
+  const handleLogin = () => {
+    const userData = {
+      id:1,
+      name:"RecepKedisi",
+      token:"123KALŞJSAK21J"
+    }
+
+    dispatch(login(userData))
+    navigate("/")
+  }
+
   return (
-    <div>Login</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <button className='px-6 py-3 bg-blue-200 transition-all rounded text-lg hover:bg-slate-400' onClick = {handleLogin}>Giriş yap</button>
+    </div>
   )
 }
 
