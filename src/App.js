9160c3ef2bff57b993c93aa15d84e137ab97b8ef
Poyraz Avsever <poyrazavsever@hotmail.com>
2022-09-17 12:55:09 +0300

    feat(auth): Authorization Work Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, NavLink, Navigate  } from "react-router-dom"
 import { routes } from "./routes"
+import {useSelector} from "react-redux"
+import React from "react";
 
 
 function App() {
 
+  const { user } = useSelector(state => state.auth)
+
   return (
+
     <div>
+
+      <nav className="h-24 flex items-center justify-center bg-slate-900">
+           
+        {routes.filter((route) => {
+          if(user && (route.path === "/login" || route.path === "/register")){
+            return false  
+          }else if(!user && (route.path === "/profile")){
+            return false
+          }else{
+            return true
+          }
+        }).map((route) => {
+
+          return (
+
+            <NavLink end exact={route.exact.toString()} to={route.path} key={route.name} 
+            className={({ isActive }) =>
+              isActive ? "text-purple-300 pl-5" : "pl-5 text-white"
+            }>
+              {route.name}
+            </NavLink>
+
+          )
+          
+        })}
+        
+      </nav>
+
+
       <Routes>
 
         {routes.map((route) => {
 
           return (
-            <Route key= {route.path} exact ={route.exact} path={route.path} element = {<route.component />}></Route>
+            <Route key={route.path} exact={route.exact} path={route.path}
+            element={route.auth && !user ? <Navigate to="/login" /> : <route.component/> } />
           )
             
         })}
--- src/routes.js
+++ src/routes.js
@@ -8,30 +8,35 @@ import Profile from "./views/Profile"
 export const routes = [
     {
         path: "/",
+        name: "Home",
         exact : true,
         component: Home,
         auth:false
     },
     {
         path: "/about",
+        name: "About",
         exact : true,
         component: About,
         auth:false
     },
     {
         path: "/login",
+        name: "Login",
         exact : true,
         component: Login,
         auth:false
     },
     {
         path: "/register",
+        name: "Register",
         exact : true,
         component: Register,
         auth:false
     },
     {
         path: "/profile",
+        name: "Profile",
         exact : true,
         component: Profile,
         auth:true
